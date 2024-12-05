3eab419b51175b513eab448163bd30ff80227b62
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2024-12-05 17:07:52 -0500

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# my-readme-generator
+
+# Non amet aliqua cupidatat enim eu pariatur consectetur anim duis ut cillum. Mollit cillum laborum ut Lorem duis sint quis ipsum proident commodo est in. Ut sunt enim sunt id sunt ullamco incididunt. Labore non id labore eiusmod Lorem ipsum qui minim consequat. Fugiat esse ad elit ullamco ullamco tempor. Excepteur eiusmod ad nisi deserunt enim Lorem deserunt voluptate ut fugiat.
+
+![License Badge](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Description
+Non amet aliqua cupidatat enim eu pariatur consectetur anim duis ut cillum. Mollit cillum laborum ut Lorem duis sint quis ipsum proident commodo est in. Ut sunt enim sunt id sunt ullamco incididunt. Labore non id labore eiusmod Lorem ipsum qui minim consequat. Fugiat esse ad elit ullamco ullamco tempor. Excepteur eiusmod ad nisi deserunt enim Lorem deserunt voluptate ut fugiat.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Non amet aliqua cupidatat enim eu pariatur consectetur anim duis ut cillum. Mollit cillum laborum ut Lorem duis sint quis ipsum proident commodo est in. Ut sunt enim sunt id sunt ullamco incididunt. Labore non id labore eiusmod Lorem ipsum qui minim consequat. Fugiat esse ad elit ullamco ullamco tempor. Excepteur eiusmod ad nisi deserunt enim Lorem deserunt voluptate ut fugiat.
+
+## Usage
+Non amet aliqua cupidatat enim eu pariatur consectetur anim duis ut cillum. Mollit cillum laborum ut Lorem duis sint quis ipsum proident commodo est in. Ut sunt enim sunt id sunt ullamco incididunt. Labore non id labore eiusmod Lorem ipsum qui minim consequat. Fugiat esse ad elit ullamco ullamco tempor. Excepteur eiusmod ad nisi deserunt enim Lorem deserunt voluptate ut fugiat.
+
+
+## License
+
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+
+## Contributing
+Non amet aliqua cupidatat enim eu pariatur consectetur anim duis ut cillum. Mollit cillum laborum ut Lorem duis sint quis ipsum proident commodo est in. Ut sunt enim sunt id sunt ullamco incididunt. Labore non id labore eiusmod Lorem ipsum qui minim consequat. Fugiat esse ad elit ullamco ullamco tempor. Excepteur eiusmod ad nisi deserunt enim Lorem deserunt voluptate ut fugiat.
+
+## Tests
+Non amet aliqua cupidatat enim eu pariatur consectetur anim duis ut cillum. Mollit cillum laborum ut Lorem duis sint quis ipsum proident commodo est in. Ut sunt enim sunt id sunt ullamco incididunt. Labore non id labore eiusmod Lorem ipsum qui minim consequat. Fugiat esse ad elit ullamco ullamco tempor. Excepteur eiusmod ad nisi deserunt enim Lorem deserunt voluptate ut fugiat.
+
+## Questions
+If you have any questions, feel free to contact me:
+- GitHub: [jcbrooks88](https://github.com/jcbrooks88)
+- Email: [jcbrooks.tech@gmail.com](mailto:jcbrooks.tech@gmail.com)
+  
--- index.js
+++ index.js
@@ -1,123 +1,152 @@
-// TODO: Include packages needed for this application
+// Include required packages
 import * as fs from 'node:fs';
 import inquirer from 'inquirer';
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of your project:',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Provide installation instructions:',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Provide usage information:',
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Provide contribution guidelines:',
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Provide test instructions:',
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Choose a license for your project:',
-      choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub username:',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Enter your email address:',
-    },
-  ];
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
 
-  const licenseBadges = {
-    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
-    'GPLv3': '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
-    'Apache 2.0': '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
-    'BSD 3-Clause': '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
-    None: '',
-  };
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-      if (err) {
-        console.error('Error writing file:', err);
-      } else {
-        console.log('README.md generated successfully!');
-      }
-    });
+// Function to generate a license badge based on the license name
+function renderLicenseBadge(license) {
+  if (license === 'None' || !license) {
+    return '';
   }
-  
-  function generateREADME(answers) {
-    return `
-  # ${answers.title}
-  
-  ${licenseBadges[answers.license]}
-  
-  ## Description
-  ${answers.description}
-  
-  ## Table of Contents
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Contributing](#contributing)
-  - [Tests](#tests)
-  - [Questions](#questions)
-  
-  ## Installation
-  ${answers.installation}
-  
-  ## Usage
-  ${answers.usage}
-  
-  ## License
-  This project is licensed under the ${answers.license} license.
-  
-  ## Contributing
-  ${answers.contributing}
-  
-  ## Tests
-  ${answers.tests}
-  
-  ## Questions
-  If you have any questions, please contact me:
-  
-  - GitHub: [${answers.github}](https://github.com/${answers.github})
-  - Email: [${answers.email}](mailto:${answers.email})
+  return `![License Badge](https://img.shields.io/badge/License-${encodeURIComponent(license)}-blue.svg)`;
+}
+
+// Function to generate the license section based on the license name
+function renderLicenseSection(license) {
+  if (license === 'None' || !license) {
+    return '';
+  }
+  const licenseLink = renderLicenseLink(license);
+  return `
+## License
+
+This project is licensed under the [${license}](${licenseLink}) license.
+`;
+}
+
+// Function to generate a license link based on the license name
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'GPLv3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'BSD 3-Clause':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    default:
+      return '';
+  }
+}
+
+// Function to generate README content
+function generateREADME(data) {
+  return `
+# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+${data.license !== 'None' ? '- [License](#license)' : ''}
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, feel free to contact me:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: [${data.email}](mailto:${data.email})
   `;
-  }
+}
 
-// TODO: Create a function to initialize app
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('README.md generated successfully!');
+    }
+  });
+}
+
+// Function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-      const readmeContent = generateREADME(answers);
-      writeToFile('README.md', readmeContent);
-    });
-  }
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateREADME(answers);
+    writeToFile('README.md', readmeContent);
+  });
+}
 
-// Function call to initialize app
+// Initialize app
 init();
+
